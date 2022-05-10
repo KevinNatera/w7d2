c4ec16bcaa849d227e836fae33e2db2f695e7083
Kevin <NateraKevin1@gmail.com>
2022-05-10 17:46:45 -0400

    added users_controller with new,create, and show methods

---
+++ music_app/app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+    def new 
+        @user = User.new
+        render :new 
+    end
+
+    def show 
+        @user = User.find(params[:id])
+        render :show
+    end
+
+    def create 
+        @user = User.new(user_params)
+
+        if @user.save
+            #login
+            redirect_to user_url(@user.id)
+        else  
+            render json: @user.errors.full_messages, status: 422
+        end
+    end
+
+
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
+
+end
