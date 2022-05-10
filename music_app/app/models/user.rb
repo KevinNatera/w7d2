6e198407a712ec20943bc4583aee4ac06aa87a2c
Kevin <NateraKevin1@gmail.com>
2022-05-10 17:24:03 -0400

    ensured session token

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -1,2 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+    validates :email,:password_digest,:session_token, presence:true 
+    validates :email, :session_token, uniqueness:true 
+
+    attr_reader :password 
+    before_validation :ensure_session_token 
+
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+
 end
