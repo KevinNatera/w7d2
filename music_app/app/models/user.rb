aec38021c002ed10f0d4f90ec90418c91e5455d3
Kevin <NateraKevin1@gmail.com>
2022-05-10 17:31:46 -0400

    added find_by_credentials method

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -16,9 +16,25 @@ class User < ApplicationRecord
     attr_reader :password 
     before_validation :ensure_session_token 
 
+    def self.find_by_credentials(email,password)
+        user = User.find_by(email: email)
+
+        if user && user.is_password?(password)
+            user
+        else   
+            nil
+        end
+
+    end 
+
     def password=(password)
         self.password_digest = BCrypt::Password.create(password)
         @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
     end
 
     def reset_session_token!
@@ -31,7 +47,10 @@ class User < ApplicationRecord
         self.session_token ||= SecureRandom::urlsafe_base64
     end
 
-
+    def self.generate_session_token
+        token = SecureRandom::urlsafe_base64
+        token
+    end
 end
 
 
